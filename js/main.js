2e075e4c3474fd731a762c23f915500e3b17ac24
ikspass <moolingtoon@mail.ru>
2026-09-21 12:11:53 +0300

    feat: add slider

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ function changeTheme() {
   if (activeTheme) {
     if (activeTheme === 'dark') {
       localStorage.setItem('theme', 'light');
-      setFirstTheme();
+      resetTheme();
     } else {
       localStorage.setItem('theme', 'dark'); 
-      setFirstTheme();
+      resetTheme();
     }
   }
   console.log(localStorage.getItem('theme'))
 }
 
-function setFirstTheme() {
+function resetTheme() {
   let activeTheme = localStorage.getItem('theme');
   if (!activeTheme) {
     localStorage.setItem('theme', 'light');
@@ -26,4 +26,49 @@ function setFirstTheme() {
   }
 }
 
-setFirstTheme();
+resetTheme();
+
+let slides = [];
+let currentSlide = 0;
+const sliderIndicators = document.querySelector('#sliderIndicators');
+
+fetch('./js/special-offers.json')
+  .then(response => response.json())
+  .then(data => {
+    slides = data;
+    sliderIndicators.innerHTML = `<div class="carousel__indicator"></div>`.repeat(slides.length)
+
+    document.querySelectorAll('.carousel__indicator')[0].classList.add('active');
+  })
+
+const leftButton = document.querySelector('#leftButton');
+const rightButton = document.querySelector('#rightButton');
+const slideImg = document.querySelector('#slideImg');
+const slideTitle = document.querySelector('#slideTitle');
+const slideDescription = document.querySelector('#slideDescription');
+
+leftButton.addEventListener('click', () => {
+  if (currentSlide - 1 < 0) currentSlide = slides.length - 1;
+  else currentSlide -= 1;
+  resetSlide();
+})
+
+rightButton.addEventListener('click', () => {
+  if (currentSlide + 1 >= slides.length) currentSlide = 0;
+  else currentSlide += 1;
+  resetSlide();
+})
+
+function resetSlide() {
+  slideImg.src = `assets/images/${slides[currentSlide].img}`;
+  slideTitle.textContent = slides[currentSlide].name;
+  slideDescription.textContent = slides[currentSlide].description;
+  resetIndicator();
+}
+
+function resetIndicator() {
+  document.querySelectorAll('.carousel__indicator').forEach(elem => {
+    elem.classList.remove('active')
+  })
+  document.querySelectorAll('.carousel__indicator')[currentSlide].classList.add('active');
+}
